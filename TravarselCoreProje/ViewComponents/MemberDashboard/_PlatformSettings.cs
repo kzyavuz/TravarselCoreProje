4d7d68ef27e4c1c47ca602dd5a12b19976840597
YAVUZ KOZ <1200505069@ogr.klu.edu.tr>
2024-05-01 12:14:37 +0300

    Update User Panel And Create Admin Panel

---
+++ TravarselCoreProje/ViewComponents/MemberDashboard/_PlatformSettings.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravarselCoreProje.ViewComponents.MemberDashboard
+{
+    public class _PlatformSettings:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
